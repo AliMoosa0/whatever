baaa15043b2682601a892e299ea423392540adbc
ALi Moosa <201902206@student.polytechnic.bh>
2022-11-08 15:13:33 +0300

    Initial Commit

---
+++ whatever/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whatever
+//
+//  Created by ALI MOOSA on 08/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
